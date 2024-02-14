d62e1f7ac9f6ba332132fdcd6e0557ff58ad0972
kndunlap <kylendunlap19@gmail.com>
2024-02-14 11:01:46 -0700

    made this all into a function! Nice!!

--- code.R
+++ code.R
@@ -1,9 +1,21 @@
-# hello just testing on my PC -
+library(tidyverse)
+library(rvest)
+# Import Most of the Stats ------------------------------------------------
 
-wsox_2023 <- "https://www.baseball-reference.com/teams/CLE/2023.shtml"
-html_most <- read_html(wsox_2023)
+import_clean <- function(team, year) {
+  random <- sample(4:7)
+  Sys.sleep(random)
+  team <- ensym(team)
+  team <- as.character(team)
+  year <- as.character(year)
+  team <- toupper(team)
+  
+bbref_url <- paste0("https://www.baseball-reference.com/teams/", team, "/", year, ".shtml")
+espn_url <- "https://www.espn.com/mlb/team/stats/_/name/cle/season/2023/seasontype/2"
+html_bbref <- read_html(bbref_url)
+html_espn <- read_html(espn_url)
 
-bbref_tables <- html_most |>
+bbref_tables <- html_bbref |>
   html_table()
 hitters <- bbref_tables[[1]]
 
@@ -27,12 +39,10 @@ hitters <- hitters |>
   slice_head(n = -4) |>
   rename(Doubles = `2B`,
          Triples = `3B`, 
-         OPS_Plus = `OPS+`) 
+         OPS_Plus = `OPS+`) |>
+  arrange(desc(OPS_Plus))
 
-tigers_url <- "https://www.espn.com/mlb/team/stats/_/name/cle/season/2023/seasontype/2"
-html_less <- read_html(tigers_url)
-
-tables <- html_less |>
+tables <- html_espn |>
   html_table()
 
 table <- cbind(tables[[1]], tables[[2]])
@@ -50,6 +60,18 @@ combined <- hitters |>
     BB = BB.x,
     SO = SO.x) |>
   select(!Name.y:BB.y)
+
+return(combined)
+
+
+}
+
+import_clean(det, 2023)
+
+
+
+# Graph -------------------------------------------------------------------
+
 combined |>
   ggplot(aes(x = reorder(Name, HR), y = HR, fill = WAR)) + geom_bar(stat = 'identity') + coord_flip() + xlab(" ") +
   scale_fill_gradient(
@@ -66,3 +88,16 @@ combined |>
                                                        legend.justification = c("right", "top"),
                                                        legend.box.just = "right",
                                                        legend.margin = margin(6, 6, 6, 6))
+
+
+import <- function(team, year) {
+  team <- ensym(team)
+  team <- as.character(team)
+  year <- as.character(year)
+  team <- toupper(team)
+  
+  wsox_2023 <- paste0("https://www.baseball-reference.com/teams/", team, "/", year, ".shtml")
+  return(wsox_2023)
+}
+
+import(det, 2023)

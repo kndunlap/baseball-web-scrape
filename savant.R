d7da2e38288b62564f77744d3ea4578c99572821
kndunlap <kylendunlap19@gmail.com>
2024-02-23 15:43:36 -0700

    so frustrating today...but I did it

--- savant.R
+++ savant.R
@@ -7,10 +7,10 @@ library(rvest)
 # Scrape Baseball Savant --------------------------------------------------
 
 
-savant <- function(savant_num, bbref_team) {
+savant <- function(savant_num, team_name) {
+team_name <- ensym(team_name)
+team <- as.character(team_name)
 Sys.sleep(2)
-bbref_team <- ensym(bbref_team)
-team_name <- as.character(bbref_team)
   
 
 savant_url <- paste0("https://baseballsavant.mlb.com/team/", savant_num)
@@ -41,7 +41,7 @@ htable_clean <- htable_final |>
   ) |>
   relocate(Season, .after = Player) |>
   mutate(
-    Team = team_name
+    Team = team
   ) |>
   relocate(Team, .after = Season) |>
   slice(1:(n() - 2)) |>
@@ -58,37 +58,92 @@ htable_clean <- htable_final |>
     Pitches = sub(",", "", Pitches)
     ) |>
   select(!`NA`) |>
-  mutate_at(vars(3:52), as.numeric)
+  mutate_at(vars(4:52), as.numeric)
 
 return(htable_clean)
 
 }
 
-savant(110, HOU)
 
 # Scrape Baseball Reference -----------------------------------------------
 
-bbref <- function(bbref_team) {
-  Sys.sleep(2)
-  bbref_team <- ensym(bbref_team)
-  team_name <- as.character(bbref_team)
-  bbref_url <- paste0("https://www.baseball-reference.com/teams/", bbref_team, "/2023.shtml")
-  
-  html_bbref <- read_html(bbref_url)
-  bbref_table <- html_bbref |>
-    html_table() 
-  
-  bbref_h <- bbref_table[[1]] |>
-    mutate(
-      Player = Name
-    ) |>
-    select(Player, PA, SB, CS, GDP, HBP, IBB, SH, `OPS+`, SF) |>
-    filter(PA > 1) |>
-    mutate(
-      Stance = case_when(
-        grepl("\\*", Player) ~ "L",
-        grepl("#", Player) ~ "S",
-        .default = "R")
+
+
+bbref(DET) |>
+  print(n = Inf)
+
+
+# Call Savant Functions ----------------------------------------------------------
+
+### AL EAST
+BALs <- savant(110, BAL)
+BOSs <- savant(111, BOS) 
+NYYs <- savant(147, NYY)
+TBRs <- savant(139, TBR)
+TORs <- savant(141, TOR) 
+
+### AL CENTRAL
+CLEs <- savant(114, CLE)
+DETs <- savant(116, DET) 
+KCs  <- savant(118, KC) 
+CHWs <- savant(145, CHW)
+MINs <- savant(142, MIN)
+
+### AL WEST
+LAAs <- savant(108, LAA)
+SEAs <- savant(136, SEA) 
+OAKs <- savant(133, OAK) 
+TEXs <- savant(140, TEX) 
+HOUs <- savant(117, HOU)
+
+### NL EAST
+WSNs <- savant(120, WSN)
+NYMs <- savant(121, NYM) 
+MIAs <- savant(146, MIA) 
+ATLs <- savant(144, ATL) 
+PHIs <- savant(143, PHI)
+
+### NL CENTRAL
+MILs <- savant(158, MIL)
+CHCs <- savant(112, CHC) 
+PITs <- savant(134, PIT) 
+CINs <- savant(113, CIN) 
+STLs <- savant(138, STL)
+
+### NL WEST
+ARIs <- savant(109, ARI)
+COLs <- savant(115, COL)
+LADs <- savant(119, LAD)
+SFs  <- savant(137, SFG) 
+SDPs <- savant(135, SDP)
+
+allteams_savant <- rbind(ARIs, ATLs, BALs, BOSs, CHCs, CHWs, CINs, CLEs, COLs, DETs, HOUs, KCs, LAAs, 
+                         LADs, MIAs, MILs, MINs, NYMs, NYYs, OAKs, PHIs, PITs, SDPs, SEAs, SFs, STLs, TBRs, TEXs, TORs, WSNs) |>
+  rename(Player = Player) |>
+  arrange(Player)
+
+allteams_names <- rbind(ARIs, ATLs, BALs, BOSs, CHCs, CHWs, CINs, CLEs, COLs, DETs, HOUs, KCs, LAAs, 
+                         LADs, MIAs, MILs, MINs, NYMs, NYYs, OAKs, PHIs, PITs, SDPs, SEAs, SFs, STLs, TBRs, TEXs, TORs, WSNs) |>
+  select(!Team) |>
+  rename(Player = Player) |>
+  arrange(Player) |>
+  select(c("Player", "Pitches", "OBP", "WOBA"))
+
+
+# Baseball Reference ------------------------------------------------------
+
+
+allteams_bbref <- read_csv("bbref.csv") |>
+  slice(-1) |>
+  rename(Age = ...2) |>
+  rename(Team = Tm) |>
+  rename(Player = Name) |>
+  filter(PA > 1) |>
+  mutate(
+    Stance = case_when(
+      grepl("\\*", Player) ~ "L",
+      grepl("#", Player) ~ "S",
+      .default = "R")
     ) |>
     mutate(
       Player = sub("\\*", "", Player)
@@ -96,100 +151,70 @@ bbref <- function(bbref_team) {
     mutate(
       Player = sub("\\#", "", Player)
     ) |>
-    filter(!Player %in% c("Team Totals", "Name")) |>
-    mutate_at(vars(2:8), as.numeric)
-  return(bbref_h)
-}
-
-bbref(DET) |>
-  print(n = Inf)
-
-
-# Join Baseball Reference and Savant --------------------------------------
+  group_by(Player) |>
+  filter(Team == "TOT" | !duplicated(Player)) |>
+  arrange(Player) |>
+  ungroup() |>
+  rename(Player = Player) |>
+  select(!Team) |>
+  select(!PA)
 
 
-### AL EAST
-BAL <- savant(110, BAL)
-BOS <- savant(111, BOS)
-NYY <- savant(147, NYY)
-TBR <- savant(139, TBR)
-TOR <- savant(141, TOR)
+# Import fWAR -------------------------------------------------------------
+fWAR <- read_csv("fangraphs WAR.csv") |>
+  mutate(Player = Name) |>
+  select(!Name)
 
-### AL CENTRAL
-CLE <- savant(114, CLE)
-DET <- savant(116, DET)
-KC  <- savant(118, KC)
-CHW <- savant(145, CHW)
-MIN <- savant(142, MIN)
+# Join --------------------------------------------------------------------
 
-### AL WEST
-LAA <- savant(108, LAA)
-SEA <- savant(136, SEA)
-OAK <- savant(133, OAK)
-TEX <- savant(140, TEX)
-HOU <- savant(117, HOU)
+# Apply gsub to remove non-alphanumeric characters
+allteams_savant$Player <- gsub("[^[:alnum:]]", "", allteams_savant_clean$Player)
+allteams_bbref$Player <- gsub("[^[:alnum:]]", "", allteams_bbref_clean$Player)
 
-### NL EAST
-WSN <- savant(120, WSN)
-NYM <- savant(121, NYM)
-MIA <- savant(146, MIA) 
-ATL <- savant(144, ATL) 
-PHI <- savant(143, PHI)
+allteams <- allteams_savant |>
+  left_join(allteams_bbref) |>
+  left_join(allteams_names, by = c("OBP", "WOBA", "Pitches")) |>
+  relocate(Player.y, .after = Player.x) |>
+  select(!Player.x) |>
+  rename(Player = Player.y) |>
+  relocate(Team, .after = Player) |>
+  left_join(fWAR, join_by(Player)) |>
+  relocate(fWAR, .after = Season) |>
+  relocate(Age, .after = Season) |>
+  relocate(Stance, .after = Season)
 
-### NL CENTRAL
-MIL <- savant(158, MIL)
-CHC <- savant(112, CHC) 
-PIT <- savant(134, PIT) #
-CIN <- savant(113, CIN) 
-STL <- savant(138, STL)
 
-### NL WEST
-ARI <- savant(109, ARI)
-COL <- savant(115, COL)
-LAD <- savant(119, LAD)
-SF  <- savant(137, SFG) #
-SDP <- savant(135, SDP)
+### MASTER SHEET
+write.csv(allteams, "final_datasetwithWAR.csv")
 
-allteams <- rbind(ARI, ATL, BAL, BOS, CHC, CHW, CIN, CLE, COL, DET, HOU, KC, LAA, LAD, MIA, MIL, MIN, NYM, NYY, OAK, PHI, PIT, SDP, SEA, SF, STL, TBR, TEX, TOR, WSN)
 
-### More cleanup after the fact
-allteams <- allteams |>
-  mutate(OPS = SLG + OBP) |>
-  mutate(
-    `Pitches/AB` = Pitches/AB
-  ) |>
-  relocate(BABIP, .after = Team) |>
-  relocate(OPS, .after = Team) |>
-  mutate_at(vars("BBE"), as.numeric)
-
+# Graphing ----------------------------------------------------------------
 
 allteams |>
   filter(PA > 50) |>
-  ggplot(aes(x = `Meatball Swing %`, y = OPS)) + 
+  ggplot(aes(x = `Sweet Spot %`, y = `OPS+`)) + 
   geom_point() +
-  geom_text_repel(
-    aes(label = Player)
-  ) +
   geom_smooth(method = "lm") 
   
-model <- lm(OPS ~ `Meatball Swing %`, data = allteams)
+model <- lm(`OPS+` ~ `Meatball Swing %`, data = allteams)
 summary(model)
 
 
-output <- sapply(allteams[, 4:ncol(allteams)], function(column) {
-  result <- lm(column ~ OPS, data = allteams)
-  coef(summary(result))["OPS", "Pr(>|t|)"]
+output <- sapply(allteams[, 6:ncol(allteams)], function(column) {
+  result <- lm(column ~ `OPS+`, data = allteams)
+  coef(summary(result))["`OPS+`", "Pr(>|t|)"]
 })
 
 ### See what correlates best with OPS (answer: sweet spot %)
 
 output <- c()
-for (i in c(4:ncol(allteams))) {            
-  output[[i]] <- cor(allteams[,4], allteams[,i])
+for (i in c(6:ncol(allteams))) {            
+  output[[i]] <- cor(allteams$`OPS+`, allteams[,i], use = "complete.obs")
 }
 output <- output[-4]
 output <- output[-3]
 output <- output[-2]
+output <- output[-1]
 output <- output[-1]
 output_df <- data.frame(output)
 
@@ -217,10 +242,8 @@ allteams |>
   )
 
 
-### fWAR
-fWAR <- read_csv("fangraphs WAR.csv") |>
-  mutate(Player = Name) |>
-  select(!Name)
+
+
 
 allteams_war <- allteams |>
   inner_join(fWAR, join_by(Player)) |>
@@ -229,3 +252,15 @@ allteams_war <- allteams |>
 
 ### load this in every time
 write_csv(allteams_war, "allteams_war.csv")
+
+
+
+allteams_savant_clean <- allteams_savant %>%
+  mutate(Player = tolower(trimws(Player)))  # Convert to lowercase and remove leading/trailing spaces
+
+allteams_bbref_clean <- allteams_bbref %>%
+  mutate(Player = tolower(trimws(Player)))  # Convert to lowercase and remove leading/trailing spaces
+
+# Attempt the join operation again
+allteams <- allteams_savant_clean %>%
+  left_join(allteams_bbref_clean, by = "Player")
